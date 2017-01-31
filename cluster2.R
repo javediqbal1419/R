fc8cac82c417de2a542529c3a71ab1f20722a9fa
javedhasan676 <javedhasan676@gmail.com>
2017-01-31 16:50:36 +0500

    2nd commit

---
+++ cluster2.R
@@ -0,0 +1,7 @@
+a = read.delim('http://ucanalytics.com/blogs/wp-content/uploads/2017/01/Cluster-Analysis-Data.txt',sep = ",",header = T)
+install.packages("fpc")
+require(fpc) 
+kmeans = kmeansruns(a,3,runs=100) 
+plot(a,col=kmeans$cluster,pch=16) 
+legend(-3,23,c('cluster 1','cluster 2','cluster 3'),pch= 16,col=c("black","green","red"))
+kmeans1=kmeansruns(a,krange = 2:10,runs=100)
